04601b42df5263a1956eab63fd2ba549683882e1
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2025-02-15 11:05:50 -0300

    Move app file to App folder

---
+++ MoviesLib/App/MoviesLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesLibApp.swift
+//  MoviesLib
+//
+//  Created by Thobias Stahlschmidt on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MovieDetailView()
+        }
+    }
+}
